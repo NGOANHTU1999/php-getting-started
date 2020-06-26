a4d13338fe4d55049c824ceb9113ad58c94464ba
NGOANHTU1999 <66111137+NGOANHTU1999@users.noreply.github.com>
2020-06-26 09:54:25 +0700

    a

---
+++ detail.php
@@ -0,0 +1,37 @@
+<?php
+include 'inc/header.php';
+?>
+ <div class="main">
+    <div class="content">
+    	<div class="section group">
+				<div class="cont-desc span_1_of_2">				
+					<div class="grid images_3_of_2">
+						<img src="https://p0ct8ommu0.vcdn.com.vn/media/catalog/product/cache/95cb36d3254e0a20b33361b06e7c0ce9/5/5/5588-606_1.jpg" alt="" />
+					</div>
+				<div class="desc span_3_of_2">
+					<h2>Future supercar 2050 </h2>
+					<p>SKU 5588-606/W</p>					
+					<div class="price">
+						<p>Price: <span>350.000 VNĐ</span></p>
+						<p>Brand:<span>VECTO</span></p>
+					</div>
+				<div class="add-cart">
+					<form action="cart.php" method="post">
+						<input type="number" class="buyfield" name="" value="1"/>
+						<input type="submit" class="buysubmit" name="submit" value="Buy Now"/>
+					</form>				
+				</div>
+			</div>
+			<div class="product-desc">
+			<h2>Product Details</h2>
+			<p>Control cars coming from the future 2050 use rechargeable batteries with batteries available in the box, know how to turn around 360 degrees</p>
+	    </div>
+				
+	</div>
+ 		</div>
+ 	</div>
+</body>
+</php>
+<?php
+include 'inc/footer.php';
+?>
